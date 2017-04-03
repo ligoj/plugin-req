5ffb805509860ea90390159c2690a10ba8a2a76c
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-04-03 09:01:04 +0200

    Initialization

---
+++ src/main/java/org/ligoj/app/plugin/req/ReqResource.java
@@ -0,0 +1,26 @@
+package org.ligoj.app.plugin.req;
+
+import org.ligoj.app.resource.plugin.AbstractServicePlugin;
+import org.springframework.stereotype.Component;
+
+/**
+ * The Requirement service.
+ */
+@Component
+public class ReqResource extends AbstractServicePlugin {
+
+	/**
+	 * Plug-in key.
+	 */
+	public static final String SERVICE_URL = BASE_URL + "/req";
+
+	/**
+	 * Plug-in key.
+	 */
+	public static final String SERVICE_KEY = SERVICE_URL.replace('/', ':').substring(1);
+
+	@Override
+	public String getKey() {
+		return SERVICE_KEY;
+	}
+}
---
+++ src/main/java/org/ligoj/app/plugin/req/ReqServicePlugin.java
@@ -0,0 +1,11 @@
+package org.ligoj.app.plugin.req;
+
+import org.ligoj.app.api.ServicePlugin;
+
+/**
+ * Features of Requirement implementations.
+ */
+public interface ReqServicePlugin extends ServicePlugin {
+
+	// Nothing to do for now
+}
