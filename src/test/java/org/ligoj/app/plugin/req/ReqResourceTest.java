58f1d7135f9182ba68a0f66284ad6fd34da4092e
Fabrice Daugan <fabrice.daugan@gmail.com>
2025-02-12 09:35:18 +0100

    bump: plugin-api 4.1.11

--- src/test/java/org/ligoj/app/plugin/req/ReqResourceTest.java
+++ src/test/java/org/ligoj/app/plugin/req/ReqResourceTest.java
@@ -33,7 +33,7 @@ public class ReqResourceTest extends AbstractAppTest {
 
 	@BeforeEach
 	void prepareData() throws IOException {
-		persistEntities("csv", new Class[]{Node.class}, StandardCharsets.UTF_8);
+		persistEntities("csv", new Class<?>[]{Node.class}, StandardCharsets.UTF_8);
 	}
 
 	@Test
